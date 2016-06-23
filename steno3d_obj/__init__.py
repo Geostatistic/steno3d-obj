c95c13cca01b22e0e5daa8339a00fefadf3b1d3d
Franklin Koch <franklin@3ptscience.com>
2016-06-23 07:45:58 -0600

    Bump version 0.0.1 -> 0.0.2

--- steno3d_obj/__init__.py
+++ steno3d_obj/__init__.py
@@ -1,6 +1,6 @@
 from .parser import obj
 
-__version__   = '0.0.1'
+__version__   = '0.0.2'
 __author__    = '3point Science, Inc.'
 __license__   = 'MIT'
 __copyright__ = 'Copyright 2016 3point Science, Inc.'
